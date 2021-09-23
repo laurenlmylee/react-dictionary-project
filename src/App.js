b0b73e0609277fd46d3d8da4a06811321a811f9d
laurenlmylee <85115625+laurenlmylee@users.noreply.github.com>
2021-09-23 11:42:13 +0100

    Set up Dictionary component, added some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-   Hello
-        
-      </header>
+      <div className="container">
+        <header className="App-header">
+          Hello
+        </header>
+        <main> 
+          <Dictionary/>
+        </main>
+        <footer className="text-center">
+          <small>Coded by {" "}
+          <a href="https://www.instagram.com/laurenlmy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >Lauren</a>🐧, <a href="https://flamboyant-heisenberg-477fb5.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-link"
+        >open-sourced </a>
+        </small>
+        </footer>
+      </div>
     </div>
   );
 }
