7c706aff2d39d391473af37869fcde3395ecec0a
laurenlmylee <85115625+laurenlmylee@users.noreply.github.com>
2021-09-23 15:08:19 +0100

    Rendered Meanings component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(" ");
+    let [results,setResults]=useState(null);
 
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+      setResults(response.data[0]);
+        // console.log(response.data[0].meanings[0].definitions[0].definition);
     }
 
     function search(event) {
@@ -25,6 +28,7 @@ export default function Dictionary() {
             <form onSubmit={search} onChange={handleKeywordChange}>
                 <input type="search" autoFocus={true} />
             </form>
+            <Results results={results}/>
         </div>
     )
 }
